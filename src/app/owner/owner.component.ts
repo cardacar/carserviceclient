a7b51e887ff76eff9dca205a153b3c7c0e3ab1ab
Carlos Quiros <carlosquiros12@gmail.com>
2021-04-22 23:36:49 -0500

    Agregando comentarios

--- src/app/owner/owner.component.ts
+++ src/app/owner/owner.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { GiphyService } from '../shared/giphy/giphy.service';
 import { NgForm } from '@angular/forms';
 
 import { OwnerService } from '../shared/owner/owner.service';
@@ -16,7 +15,6 @@ export class OwnerComponent{
 
   owner: any = {};
   sub: Subscription;
-  //ownerSelected: Owner;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -62,51 +60,5 @@ export class OwnerComponent{
       this.gotoList();
     }, error => console.error(error));
   }
-
-  /* eventSelection(event){
-    this.ownerSelected = event.name;
-   } */
-
-  /* mapOnwerInView(id) {
-    if (id) {
-      console.log(id);
-      this.ownerService.get(id).subscribe((owner: any) => {
-        console.log(owner)
-        if (owner) {
-          this.owner = owner;
-          this.owner.href = owner._links.self.href;
-          // this.showCarOwnerInSelect();
-        } else {
-          console.log(`owner with id '${id}' not found, returning to list`);
-          this.gotoList();
-        }
-      });
-    }
-  } */
-
-
-
-  
-
-  /* ngOnDestroy1() {
-    this.sub.unsubscribe();
-  }
-
-  gotoList1() {
-    this.router.navigate(['/owners-list']);
-  }
-
-  save1(form: NgForm) {
-    this.ownerService.save(form).subscribe(result => {
-      this.gotoList();
-    }, error => console.error(error));
-  }
-
-  remove1(href) {
-    this.ownerService.remove(href).subscribe(result => {
-      console.log(result);
-      this.gotoList();
-    }, error => console.error(error));
-  } */
 
 }
